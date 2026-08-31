df5ec071a90ca3504cbbf508b6a70f5ed8a92e47
susana0204 <suuuuuu0204@gmail.com>
2026-08-31 10:23:18 -0300

    test usuario

--- Sistemadealumnos/Program.cs
+++ Sistemadealumnos/Program.cs
@@ -18,4 +18,4 @@ Console.WriteLine(alumno2.Legajo);
 
 alumno1.Nombre = "Franco";
 Console.WriteLine(alumno1.Nombre);
-Console.WriteLine(alumno2.Nombre);//Etapa 1 — La primera clase
+Console.WriteLine(alumno2.Nombre);//Etapa 1 — La primera clase - test
