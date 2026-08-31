4f28c1be341c68eeffbe27479df22e70076ec37f
sushiii <sushiii@sushiiiii-2.local>
2026-08-31 10:21:01 -0300

    etapa-1

---
+++ Sistemadealumnos/Alumno.cs
@@ -0,0 +1,9 @@
+namespace Sistemadealumnos;
+
+public class Alumno
+{
+   public string Nombre { get; set; }
+   public  int Legajo { get; set; }
+   public decimal Nota1 { get; set; }
+   public decimal Nota2 { get; set; }
+}
--- Sistemadealumnos/Program.cs
+++ Sistemadealumnos/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Sistemadealumnos;
+
+Alumno alumno1= new Alumno()
+{
+    Nombre = "Julian",
+    Legajo = 12345,
+};
+Alumno alumno2 = new Alumno()
+{
+    Nombre = "Candela",
+    Legajo = 67890,
+};
+
+Console.WriteLine(alumno1.Nombre);
+Console.WriteLine(alumno1.Legajo);
+Console.WriteLine(alumno2.Nombre);
+Console.WriteLine(alumno2.Legajo);
+
+alumno1.Nombre = "Franco";
+Console.WriteLine(alumno1.Nombre);
+Console.WriteLine(alumno2.Nombre);//Etapa 1 — La primera clase
